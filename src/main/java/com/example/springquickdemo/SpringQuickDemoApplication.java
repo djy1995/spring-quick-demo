094d3c8242faf836b71d798cead2a5712cfb8b4a
djy <940970549@qq.com>
2020-03-15 22:39:57 +0800

    first commit;

---
+++ src/main/java/com/example/springquickdemo/SpringQuickDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springquickdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuickDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuickDemoApplication.class, args);
+    }
+
+}
